304b6ee3b34818c9bae93426622c6c759b8b521d
RockCrafts <ryantanenholz@icloud.com>
2020-08-02 17:08:25 -0400

    Player Updates & Tile Assets

---
+++ GameJam20202D/Assets/Scripts/RewindCore.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RewindCore : MonoBehaviour
+{
+    private bool rewinding = false;
+    List<TimePoint> points;
+    Rigidbody2D rb;
+    public float timeToRecord = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        points = new List<TimePoint>();
+        if (GetComponent<Rigidbody2D>() == null)
+        {
+            rb = null;
+        }
+        else
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (rewinding)
+        {
+            Rewind();
+        }
+        else
+        {
+            Record();
+        }
+    }
+    void Rewind()
+    {
+        if (points.Count > 0)
+        {
+            TimePoint tp = points[0];
+            transform.position = tp.position;
+            transform.rotation = tp.rotation;
+            points.RemoveAt(0);
+        }
+        else
+        {
+            stopRewind();
+        }
+    }
+    [ContextMenu("startRewind")]
+    public void startRewind() {
+        rewinding = true;
+        if (rb == null) return;
+        rb.isKinematic = true;
+    }
+    private void stopRewind()
+    {
+        rewinding = false;
+        if (rb == null) return;
+        rb.isKinematic = false;
+    }
+    void Record()
+    {
+        if (points.Count > Mathf.Round(timeToRecord / Time.fixedDeltaTime))
+        {
+            points.RemoveAt(points.Count - 1);
+        }
+        points.Insert(0, new TimePoint(transform.position, transform.rotation));
+
+    }
+}
---
+++ GameJam20202D/Assets/Scripts/TimePoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimePoint
+{
+    public Quaternion rotation;
+    public Vector3 position;
+
+    public TimePoint(Vector3 _position, Quaternion _rotation)
+    {
+        rotation = _rotation;
+        position = _position; 
+    }
+}
