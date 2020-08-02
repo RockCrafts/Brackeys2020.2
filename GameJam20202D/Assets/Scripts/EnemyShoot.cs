c4d8cf6149debce899abe35e2e7c85451dd019db
zevgaslin <zevgaslin@gmail.com>
2020-08-02 14:59:41 -0700

    started enmey shoot

---
+++ GameJam20202D/Assets/Scripts/EnemyShoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour
+{
+    GameObject player;
+    public float minTurn = -10f;
+    public float maxTurn = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector2 playerPos = new Vector2(player.transform.position.x, player.transform.position.y + 5);
+        Debug.DrawRay(this.transform.position, player.transform.position, Color.red);
+        Physics2D.Raycast(this.transform.position, player.transform.position);
+
+    }
+}
