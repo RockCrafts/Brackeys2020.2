ced302ce43a9c587d13853e0ad2e2766a9109b33
RockCrafts <ryantanenholz@icloud.com>
2020-08-02 00:23:12 -0400

    RunAnimation Setup + Player Controller

---
+++ GameJam20202D/Assets/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    SpriteRenderer sr;
+    Animator a;
+    
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        a = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+       a.SetFloat("Direction", x);
+        if (x == 0)
+        {
+           a.SetBool("Running", false);
+        }
+        else
+        {
+            if (x < 0)
+            {
+               sr.flipX = true;
+            }
+            else if (x > 0)
+            {
+               sr.flipX = false;
+            }
+           a.SetBool("Running", true);
+        }
+        
+        transform.Translate(Vector2.right * x * 5f * Time.deltaTime);
+    }
+}
