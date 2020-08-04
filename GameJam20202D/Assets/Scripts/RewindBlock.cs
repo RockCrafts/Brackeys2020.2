4b1ac1d593d752cfcfd7e1447b95ffcba6fc7231
zevgaslin <zevgaslin@gmail.com>
2020-08-03 21:01:01 -0700

    rewind Block

---
+++ GameJam20202D/Assets/Scripts/RewindBlock.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RewindBlock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("COlide");
+        if(other.gameObject.tag == "Player")
+        {
+            RewindCore rewindCore = other.gameObject.GetComponent<RewindCore>();
+            if (rewindCore.getRewinding())
+            {
+                return;
+            }
+            else
+            {
+                rewindCore.startRewind();
+
+            }
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
