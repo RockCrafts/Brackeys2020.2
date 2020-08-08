420eeb34b3216dab4422a484198110dedb40fdc2
zevgaslin <zevgaslin@gmail.com>
2020-08-07 22:36:32 -0700

    timer ish

---
+++ GameJam20202D/Assets/Scripts/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text text;
+    float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "Time: " + timer;
+        
+        if(timer == 5f)
+        {
+            timer = 0f;
+        }
+        else
+        {
+            timer += Time.deltaTime;
+        }
+
+    }
+
+}
